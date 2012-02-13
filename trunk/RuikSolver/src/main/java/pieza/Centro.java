a216080512def2159faf274646362cd5955d2254
ejtiernes <ejtiernes@f817bdb0-2947-1648-781d-03ef1b1be8d6>
2012-02-13 20:33:26 +0000

    arista etc.. cn fayos

---
+++ trunk/RuikSolver/src/main/java/pieza/Centro.java
@@ -0,0 +1,5 @@
+package pieza;
+
+public class Centro {
+
+}
---
+++ trunk/RuikSolver/src/main/java/pieza/Vertice.java
@@ -0,0 +1,11 @@
+package pieza;
+
+import java.util.Set;
+
+import color.Color;
+
+public class Vertice {
+	private final Vectr posicion;
+	private final Vectr orientacion;
+	private final Set<Color> color;
+}
