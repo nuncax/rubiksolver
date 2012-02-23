a48c71fbbd66049ca0579b12363709bad7d4bf4c
ejtiernes <ejtiernes@f817bdb0-2947-1648-781d-03ef1b1be8d6>
2012-02-23 18:38:56 +0000

    refactor con comentarios para borrarlos ahora

--- RuikSolver/src/main/java/pieza/Vectr.java
+++ RuikSolver/src/main/java/pieza/Vectr.java
@@ -1,56 +1,70 @@
 package pieza;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 public class Vectr {
-	private List<Integer> vector;
+	private int[] vector;
+	final int X = 0;
+	final int Y = 1;
 
-	public Vectr(Integer x, Integer y, Integer Z) {
-		this.vector = new ArrayList<Integer>(3);
-		this.vector.add(x);
-		this.vector.add(y);
-		this.vector.add(Z);
+	public int[] getVector() {
+		return vector;
+	}
+
+	final int Z = 2;
+
+	public Vectr(Integer x, Integer y, Integer z) {
+		this.vector = new int[3];
+		vector[X] = x;
+		vector[Y] = y;
+		vector[Z] = z;
 	}
 
 	public Vectr() {
-		// TODO Auto-generated constructor stub
+		this.vector = new int[3];
 	}
 
-	public void setCords(Integer x, Integer y, Integer z) {
-		this.vector.clear();
-		this.vector.add(x);
-		this.vector.add(y);
-		this.vector.add(z);
-	}
-
-	public void setX(Integer x) {
-		this.vector.set(0, x);
-	}
-
-	public void setY(Integer y) {
-		this.vector.set(1, y);
-	}
-
-	public void setZ(Integer z) {
-		this.vector.set(2, z);
-	}
-
-	public Integer getX() {
-		return this.vector.get(0);
-	}
-
-	public Integer getY() {
-		return this.vector.get(1);
-	}
-
-	public Integer getZ() {
-		return this.vector.get(2);
+	public void multiplicaPorMatriz(int m[][]) {
+		int[] res = new int[3];
+		for (int i = 0; i < m.length; i++) {
+			for (int j = 0; j < m.length; j++) {
+				res[i] += m[i][j] * this.vector[j];
+			}
+		}
+		this.vector = res;
 	}
 
 	@Override
 	public String toString() {
-		return vector.toString();
+		return "Vectr [vector=" + Arrays.toString(vector) + "]";
 	}
+
+	// public void setCords(Integer x, Integer y, Integer z) {
+	//
+	// }
+
+	// public void setX(Integer x) {
+	// this.vector.set(0, x);
+	// }
+	//
+	// public void setY(Integer y) {
+	// this.vector.set(1, y);
+	// }
+	//
+	// public void setZ(Integer z) {
+	// this.vector.set(2, z);
+	// }
+	//
+	// public Integer getX() {
+	// return this.vector.get(0);
+	// }
+	//
+	// public Integer getY() {
+	// return this.vector.get(1);
+	// }
+	//
+	// public Integer getZ() {
+	// return this.vector.get(2);
+	// }
 
 }
